4156f15a48e4d8a26c10d7c00dc5883183199851
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 04:47:00 -0300

    test: ensure CreateUserUseCase throws business exception if email os already taken

--- src/test/java/com/walletwise/application/useCases/implementations/CreateUserUseCaseTests.java
+++ src/test/java/com/walletwise/application/useCases/implementations/CreateUserUseCaseTests.java
@@ -1,5 +1,6 @@
 package com.walletwise.application.useCases.implementations;
 
+import com.walletwise.application.gateways.IFindUserByEmailGateway;
 import com.walletwise.application.gateways.IFindUserByUserNameGateway;
 import com.walletwise.application.useCases.contracts.ICreateUserUseCase;
 import com.walletwise.domain.entities.User;
@@ -18,15 +19,18 @@ class CreateUserUseCaseTests {
 
     @MockBean
     private IFindUserByUserNameGateway findUserByUserNameGateway;
+    @MockBean
+    private IFindUserByEmailGateway findUserByEmailGateway;
+
 
     @BeforeEach
     void setUp() {
-        createUserUseCase = new CreateUserUseCase(findUserByUserNameGateway);
+        createUserUseCase = new CreateUserUseCase(findUserByUserNameGateway, findUserByEmailGateway);
     }
 
     @Test
-    @DisplayName("Should throw business exception if username already taken")
-    void shouldThrowBusinessExceptionIfUsernameAlreadyTaken() {
+    @DisplayName("Should throw business exception if username already in use")
+    void shouldThrowBusinessExceptionIfUsernameAlreadyInUse() {
         User user = new User("any_fistname", "any_lastname", "any_saved_username", "any_email", "any_password");
         User savedUser = new User("any_saved_fistname", "any_saved_lastname", "any_saved_username", "any_saved_email", "any_saved_password");
 
@@ -37,5 +41,23 @@ class CreateUserUseCaseTests {
         Assertions.assertThat(exception).isInstanceOf(BusinessException.class);
         Assertions.assertThat(exception.getMessage()).isEqualTo("The username is already in use. Please try another username.");
         Mockito.verify(this.findUserByUserNameGateway, Mockito.times(1)).find(user.username());
+    }
+
+    @Test
+    @DisplayName("Should throw business exception if email is already in use ")
+    void shouldThrowBusinessExceptionIfEmailAlreadyInUse() {
+        User user = new User("any_fistname", "any_lastname", "any_username", "any_saved_email", "any_password");
+        User savedUser = new User("any_saved_fistname", "any_saved_lastname", "any_saved_username", "any_saved_email", "any_saved_password");
+
+        Mockito.when(this.findUserByUserNameGateway.find(user.username())).thenReturn(null);
+        Mockito.when(this.findUserByEmailGateway.find(user.email())).thenReturn(savedUser);
+
+
+        Throwable exception = Assertions.catchThrowable(() -> this.createUserUseCase.create(user));
+
+        Assertions.assertThat(exception).isInstanceOf(BusinessException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("The email is already in use. Please try another email.");
+        Mockito.verify(this.findUserByUserNameGateway, Mockito.times(1)).find(user.username());
+        Mockito.verify(this.findUserByEmailGateway, Mockito.times(1)).find(user.email());
     }
 }
