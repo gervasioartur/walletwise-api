f7b2a59c629bf8e3ac4273e5fb5535c381fa1561
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 04:46:52 -0300

    feat: ensure CreateUserUseCase throws business exception if email os already taken

--- src/main/java/com/walletwise/application/useCases/implementations/CreateUserUseCase.java
+++ src/main/java/com/walletwise/application/useCases/implementations/CreateUserUseCase.java
@@ -1,5 +1,6 @@
 package com.walletwise.application.useCases.implementations;
 
+import com.walletwise.application.gateways.IFindUserByEmailGateway;
 import com.walletwise.application.gateways.IFindUserByUserNameGateway;
 import com.walletwise.application.useCases.contracts.ICreateUserUseCase;
 import com.walletwise.domain.entities.User;
@@ -8,17 +9,23 @@ import com.walletwise.domain.exceptions.BusinessException;
 
 public class CreateUserUseCase implements ICreateUserUseCase {
     private final IFindUserByUserNameGateway findUserByUserNameGateway;
+    private final IFindUserByEmailGateway findUserByEmailGateway;
 
-    public CreateUserUseCase(IFindUserByUserNameGateway findUserByUserNameGateway) {
+    public CreateUserUseCase(IFindUserByUserNameGateway findUserByUserNameGateway, IFindUserByEmailGateway findUserByEmailGateway) {
         this.findUserByUserNameGateway = findUserByUserNameGateway;
+        this.findUserByEmailGateway =  findUserByEmailGateway;
     }
 
     @Override
     public UserAccount create(User user) {
-        User userResult = findUserByUserNameGateway.find(user.username());
-
+        User userResult = this.findUserByUserNameGateway.find(user.username());
         if (userResult != null)
             throw new BusinessException("The username is already in use. Please try another username.");
+
+        userResult =  this.findUserByEmailGateway.find(user.email());
+        if (userResult != null)
+            throw new BusinessException("The email is already in use. Please try another email.");
+
         return null;
     }
 }
