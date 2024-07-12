ff55ecfc0279658202df0cadeef86030d0c811d8
dombo99 <202204259818@alunos.estacio.br>
2024-07-12 06:47:22 -0300

    chore: add missing tests

--- src/test/java/com/walletwise/WalletWiseApplicationTests.java
+++ src/test/java/com/walletwise/WalletWiseApplicationTests.java
@@ -1,5 +1,6 @@
 package com.walletwise;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class WalletWiseApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
+    @Test
+    void mainMethodRunsSuccessfully() {
+        WalletWiseApplication.main(new String[]{});
+        Assertions.assertTrue(true);
+    }
 }
