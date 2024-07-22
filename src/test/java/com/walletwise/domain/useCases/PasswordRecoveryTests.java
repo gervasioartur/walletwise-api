3e7403a6b6b599b05078e9cd415e26fe175a8f6f
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 18:30:33 -0300

    test: ensure PasswordRecovery sends email with the instructions on password recovery success

--- src/test/java/com/walletwise/domain/useCases/PasswordRecoveryTests.java
+++ src/test/java/com/walletwise/domain/useCases/PasswordRecoveryTests.java
@@ -1,8 +1,12 @@
 package com.walletwise.domain.useCases;
 
+import com.walletwise.domain.adapters.IAuthAdapter;
+import com.walletwise.domain.adapters.ICryptoAdapter;
+import com.walletwise.domain.adapters.IEmailAdapter;
 import com.walletwise.domain.adapters.IUserAdapter;
+import com.walletwise.domain.entities.models.User;
+import com.walletwise.domain.entities.models.ValidationToken;
 import com.walletwise.mocks.Mocks;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,21 +14,31 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.UUID;
+
 @SpringBootTest
 class PasswordRecoveryTests {
     private PasswordRecovery passwordRecovery;
 
     @MockBean
     private IUserAdapter userAdapter;
+    @MockBean
+    private ICryptoAdapter cryptoAdapter;
+    @MockBean
+    private IAuthAdapter authAdapter;
+    @MockBean
+    private IEmailAdapter emailAdapter;
+    private String baseUrl =  Mocks.faker.internet().url();
 
     @BeforeEach
-    void setup(){
-        this.passwordRecovery =  new PasswordRecovery(userAdapter);
+    void setup() {
+        this.passwordRecovery = new PasswordRecovery
+                (userAdapter, cryptoAdapter,authAdapter,emailAdapter,baseUrl);
     }
 
     @Test
     @DisplayName("Should stop method execution if user does not exist by email")
-    void shouldStopMethodExecutionIfUserDoesNotExistByEmail(){
+    void shouldStopMethodExecutionIfUserDoesNotExistByEmail() {
         String email = Mocks.faker.internet().emailAddress();
 
         Mockito.when(this.userAdapter.findByEmail(email)).thenReturn(null);
@@ -32,5 +46,33 @@ class PasswordRecoveryTests {
         this.passwordRecovery.recover(email);
 
         Mockito.verify(this.userAdapter, Mockito.times(1)).findByEmail(email);
+    }
+
+    @Test
+    @DisplayName("Should send email to user with instruction to recover password")
+    void shouldSendEmailToUserWithInstructionsToRecoverPassword(){
+        User savedUser = Mocks.savedUserDomainObjectFactory();
+
+        ValidationToken savedValidationToken =  Mocks.validationTokenFactory();
+
+        String token = UUID.randomUUID().toString();
+        String encodedToken =  savedValidationToken.getToken();
+
+        String resetUrl =  this.baseUrl + "/reset-password?token=" + token;
+        String message = "Password Reset Request,\n Click the link to reset your password: " + resetUrl;
+
+        Mockito.when(this.userAdapter.findByEmail(savedUser.getEmail())).thenReturn(savedUser);
+        Mockito.when(this.cryptoAdapter.generateToken()).thenReturn(token);
+        Mockito.when(this.cryptoAdapter.encode(token)).thenReturn(encodedToken);
+        Mockito.when(this.authAdapter.saveValidationToken(Mockito.any(ValidationToken.class))).thenReturn(savedValidationToken);
+        Mockito.doNothing().when(this.emailAdapter).sendEmail(savedUser.getEmail(),message);
+
+        this.passwordRecovery.recover(savedUser.getEmail());
+
+        Mockito.verify(this.userAdapter, Mockito.times(1)).findByEmail(savedUser.getEmail());
+        Mockito.verify(this.cryptoAdapter,Mockito.times(1)).generateToken();
+        Mockito.verify(this.cryptoAdapter,Mockito.times(1)).encode(token);
+        Mockito.verify(this.authAdapter,Mockito.times(1)).saveValidationToken(Mockito.any(ValidationToken.class));
+        Mockito.verify(this.emailAdapter,Mockito.times(1)).sendEmail(savedUser.getEmail(),message);
     }
 }
