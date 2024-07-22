c915820205b447739d26c8419b7704e3341e8039
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 04:46:22 -0300

    feat: ensure Signin throws UnauthorizedException if password is wrong

--- src/main/java/com/walletwise/domain/useCases/Signin.java
+++ src/main/java/com/walletwise/domain/useCases/Signin.java
@@ -1,24 +1,32 @@
 package com.walletwise.domain.useCases;
 
+import com.walletwise.domain.adapters.IAuthAdapter;
 import com.walletwise.domain.adapters.IUserAdapter;
 import com.walletwise.domain.entities.exceptions.UnauthorizedException;
+import com.walletwise.domain.entities.models.User;
 
 public class Signin {
     private IUserAdapter userAdapter;
+    private IAuthAdapter authAdapter;
 
-    public Signin(IUserAdapter userAdapter) {
+    public Signin(IUserAdapter userAdapter, IAuthAdapter authAdapter) {
         this.userAdapter = userAdapter;
+        this.authAdapter = authAdapter;
     }
 
     public String signin(String usernameOrEmail, String password) {
+        User userResult = null;
+
         if (usernameOrEmail.contains("@")) {
-            if (this.userAdapter.findByEmail(usernameOrEmail) == null)
-                throw new UnauthorizedException("Invalid email or password.");
+           userResult =  this.userAdapter.findByEmail(usernameOrEmail);
+            if (userResult == null) throw new UnauthorizedException("Invalid email or password.");
         } else {
-            if (this.userAdapter.findByUsername(usernameOrEmail) == null)
-                throw new UnauthorizedException("Invalid username or password.");
+            userResult = this.userAdapter.findByUsername(usernameOrEmail);
+            if (userResult == null) throw new UnauthorizedException("Invalid username or password.");
         }
 
+        String result = this.authAdapter.authenticate(userResult.getUsername(),password);
+        if(result == null) throw new UnauthorizedException("Bad credentials.");
         return null;
     }
 }
