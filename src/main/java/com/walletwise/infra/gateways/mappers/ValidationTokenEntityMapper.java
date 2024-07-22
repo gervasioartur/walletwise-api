2acb4e80af6d83adacf15c0e8300956ea3b731ce
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 19:12:16 -0300

    feat: ensure ValidationTokenEntityMapper return ValidationTokenEntity

---
+++ src/main/java/com/walletwise/infra/gateways/mappers/ValidationTokenEntityMapper.java
@@ -0,0 +1,19 @@
+package com.walletwise.infra.gateways.mappers;
+
+import com.walletwise.domain.entities.models.ValidationToken;
+import com.walletwise.infra.persistence.entities.ValidationTokenEntity;
+
+public class ValidationTokenEntityMapper {
+    public ValidationTokenEntity toValidationTokenEntity(ValidationToken validationToken){
+        return ValidationTokenEntity
+                .builder()
+                .id(validationToken.getId())
+                .token(validationToken.getToken())
+                .expirationDate(validationToken.getExpirationDate())
+                .build();
+    }
+
+    public ValidationToken toValidationTokenDomainObject(ValidationTokenEntity entity){
+        return new ValidationToken(entity.getId(),entity.getToken(),entity.getExpirationDate());
+    }
+}
