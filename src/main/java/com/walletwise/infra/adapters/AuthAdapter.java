924ff70e4f4fddccc1072f430bde274b855c4e78
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 19:41:10 -0300

    feat: ensure AuthAdapter savedValidationToken return ValidationToken on save success

--- src/main/java/com/walletwise/infra/adapters/AuthAdapter.java
+++ src/main/java/com/walletwise/infra/adapters/AuthAdapter.java
@@ -2,7 +2,10 @@ package com.walletwise.infra.adapters;
 
 import com.walletwise.domain.adapters.IAuthAdapter;
 import com.walletwise.domain.entities.models.ValidationToken;
+import com.walletwise.infra.gateways.mappers.ValidationTokenEntityMapper;
 import com.walletwise.infra.gateways.token.GenerateToken;
+import com.walletwise.infra.persistence.entities.ValidationTokenEntity;
+import com.walletwise.infra.persistence.repositories.IValidationTokenEntityRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,10 +13,17 @@ import org.springframework.security.core.Authentication;
 public class AuthAdapter implements IAuthAdapter {
     private final AuthenticationManager authenticationManager;
     private final GenerateToken generateToken;
+    private final ValidationTokenEntityMapper validationTokenEntityMapper;
+    private final IValidationTokenEntityRepository validationTokenEntityRepository;
 
-    public AuthAdapter(AuthenticationManager authenticationManager, GenerateToken generateToken) {
+    public AuthAdapter(AuthenticationManager authenticationManager,
+                       GenerateToken generateToken,
+                       ValidationTokenEntityMapper validationTokenEntityMapper,
+                       IValidationTokenEntityRepository validationTokenEntityRepository) {
         this.authenticationManager = authenticationManager;
         this.generateToken = generateToken;
+        this.validationTokenEntityMapper = validationTokenEntityMapper;
+        this.validationTokenEntityRepository = validationTokenEntityRepository;
     }
 
     @Override
@@ -29,6 +39,9 @@ public class AuthAdapter implements IAuthAdapter {
 
     @Override
     public ValidationToken saveValidationToken(ValidationToken validationToken) {
-        return null;
+        ValidationTokenEntity entity = this.validationTokenEntityMapper.toValidationTokenEntity(validationToken);
+        entity.setActive(true);
+        entity = this.validationTokenEntityRepository.save(entity);
+        return this.validationTokenEntityMapper.toValidationTokenDomainObject(entity);
     }
 }
