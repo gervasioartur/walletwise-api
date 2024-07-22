4d516dc87ed1213a3464009846f8ccc0e72c1357
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 20:16:57 -0300

    feat: ensure EmailAdapter RuntimeException if send email throws

--- src/main/java/com/walletwise/domain/adapters/IEmailAdapter.java
+++ src/main/java/com/walletwise/domain/adapters/IEmailAdapter.java
@@ -1,5 +1,7 @@
 package com.walletwise.domain.adapters;
 
+import java.rmi.UnexpectedException;
+
 public interface IEmailAdapter {
-    void sendEmail(String receptor, String message);
+    void sendEmail(String receptor, String message, String subject);
 }
