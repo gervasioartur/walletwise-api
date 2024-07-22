d856a53c47476bcba94b751c9172531662d92ce7
dombo99 <202204259818@alunos.estacio.br>
2024-07-22 04:46:34 -0300

    test: ensure Signin throws UnauthorizedException if password is wrong

--- src/test/java/com/walletwise/domain/useCases/SigninTests.java
+++ src/test/java/com/walletwise/domain/useCases/SigninTests.java
@@ -1,7 +1,9 @@
 package com.walletwise.domain.useCases;
 
+import com.walletwise.domain.adapters.IAuthAdapter;
 import com.walletwise.domain.adapters.IUserAdapter;
 import com.walletwise.domain.entities.exceptions.UnauthorizedException;
+import com.walletwise.domain.entities.models.User;
 import com.walletwise.mocks.Mocks;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,14 +19,16 @@ class SigninTests {
 
     @MockBean
     private IUserAdapter userAdapter;
+    @MockBean
+    private IAuthAdapter authAdapter;
 
     @BeforeEach
     void setup() {
-        this.signin = new Signin(userAdapter);
+        this.signin = new Signin(userAdapter,authAdapter);
     }
 
     @Test
-    @DisplayName("Should throw UnauthorizedException if user does not exist by email ")
+    @DisplayName("Should throw UnauthorizedException if user does not exist by email")
     void shouldThrowUnauthorizedExceptionIfUserDoesNotExistsByEmail() {
         String email = Mocks.faker.internet().emailAddress();
         String password = Mocks.faker.internet().password();
@@ -39,7 +43,7 @@ class SigninTests {
     }
 
     @Test
-    @DisplayName("Should throw UnauthorizedException if user does not exist by username ")
+    @DisplayName("Should throw UnauthorizedException if user does not exist by username")
     void shouldThrowUnauthorizedExceptionIfUserDoesNotExistsByUsername() {
         String username = Mocks.faker.name().username();
         String password = Mocks.faker.internet().password();
@@ -50,6 +54,24 @@ class SigninTests {
 
         Assertions.assertThat(exception).isInstanceOf(UnauthorizedException.class);
         Assertions.assertThat(exception.getMessage()).isEqualTo("Invalid username or password.");
+        Mockito.verify(this.userAdapter, Mockito.times(1)).findByUsername(username);
+    }
+
+    @Test
+    @DisplayName("Should throw UnauthorizedException if password is wrong")
+    void shouldThrowUnauthorizedExceptionIfPasswordIsWrong() {
+        String username = Mocks.faker.name().username();
+        String password = Mocks.faker.internet().password();
+
+        User savedUser = Mocks.savedUserDomainObjectFactory();
+
+        Mockito.when(this.userAdapter.findByUsername(username)).thenReturn(savedUser);
+        Mockito.when(this.authAdapter.authenticate(username,password)).thenReturn(null);
+
+        Throwable exception = Assertions.catchThrowable(() -> this.signin.signin(username, password));
+
+        Assertions.assertThat(exception).isInstanceOf(UnauthorizedException.class);
+        Assertions.assertThat(exception.getMessage()).isEqualTo("Bad credentials.");
         Mockito.verify(this.userAdapter, Mockito.times(1)).findByUsername(username);
     }
 }
