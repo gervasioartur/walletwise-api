aa9590350276ab3e6c3a0a23da68527862274ff9
dombo99 <202204259818@alunos.estacio.br>
2024-07-23 11:03:59 -0300

    fix: make sure user password get encoded before saving

--- src/main/java/com/walletwise/domain/useCases/Signup.java
+++ src/main/java/com/walletwise/domain/useCases/Signup.java
@@ -1,15 +1,18 @@
 package com.walletwise.domain.useCases;
 
 
+import com.walletwise.domain.adapters.ICryptoAdapter;
 import com.walletwise.domain.adapters.IUserAdapter;
 import com.walletwise.domain.entities.exceptions.ConflictException;
 import com.walletwise.domain.entities.models.User;
 
 public class Signup {
     private final IUserAdapter userAdapter;
+    private final ICryptoAdapter cryptoAdapter;
 
-    public Signup(IUserAdapter userAdapter) {
+    public Signup(IUserAdapter userAdapter, ICryptoAdapter cryptoAdapter) {
         this.userAdapter = userAdapter;
+        this.cryptoAdapter = cryptoAdapter;
     }
 
     public void signup(User user) {
@@ -18,6 +21,9 @@ public class Signup {
 
         if (this.userAdapter.findByEmail(user.getEmail()) != null)
             throw new ConflictException("E-mail already in use.");
+
+        String encodedPassword =  this.cryptoAdapter.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         this.userAdapter.save(user);
     }
 }
