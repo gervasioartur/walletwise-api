c18c0bd1434bab19752341721db3788c787a070c
gervasioartur <gervasioartur@outlook.com.br>
2024-07-26 12:43:53 -0300

    feat: ensure FixedExpenseEntityMapper return FixedExpense

--- src/main/java/com/walletwise/infra/gateways/mappers/FixedExpenseEntityMapper.java
+++ src/main/java/com/walletwise/infra/gateways/mappers/FixedExpenseEntityMapper.java
@@ -10,10 +10,24 @@ public class FixedExpenseEntityMapper {
                 fixedExpense.getId(),
                 fixedExpense.getDescription(),
                 fixedExpense.getCategory(),
+                fixedExpense.getAmount(),
                 fixedExpense.getDueDay(),
                 fixedExpense.getPaymentFrequency(),
                 UserEntity.builder().id(fixedExpense.getUserId()).build(),
                 fixedExpense.getStartDate(),
                 fixedExpense.getEndDate());
+    }
+
+    public FixedExpense toFixedExpense(FixedExpenseEntity entity){
+        return new FixedExpense(
+                entity.getId(),
+                entity.getUser().getId(),
+                entity.getDescription(),
+                entity.getAmount(),
+                entity.getCategory(),
+                entity.getDueDay(),
+                entity.getStartDate(),
+                entity.getEndDate(),
+                entity.getPaymentFrequency());
     }
 }
