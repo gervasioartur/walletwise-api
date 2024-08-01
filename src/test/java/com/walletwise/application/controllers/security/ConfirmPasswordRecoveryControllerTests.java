a35248f343b65e0ed24318abdfd02346ce209036
gervasioartur <gervasioartur@outlook.com.br>
2024-08-01 15:25:55 -0300

    refactor: remove public in ConfirmPasswordRecoveryControllerTests class definition

--- src/test/java/com/walletwise/application/controllers/security/ConfirmPasswordRecoveryControllerTests.java
+++ src/test/java/com/walletwise/application/controllers/security/ConfirmPasswordRecoveryControllerTests.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-public class ConfirmPasswordRecoveryControllerTests {
+class ConfirmPasswordRecoveryControllerTests {
     private final String URL = "/auth/confirm-password-recovery";
     @Autowired
     private WebApplicationContext context;
